bc4505d81b4f7104a73d67fdfb75f2195eb81e16
ebbes.ebbes <ebbes.ebbes@gmail.com>
2011-10-21 13:56:55 +0000

    initial commit

---
+++ iphone-backup-browser/iTunesBackupParser/BackupInfoParser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace iTunesBackupParser
+{
+    public static class BackupInfoParser
+    {
+        public static Dictionary<string, string> ParseInfo(string backupFolder)
+        {
+            try
+            {
+                string infoFile = Path.Combine(backupFolder, "Info.plist");
+                if (!File.Exists(infoFile))
+                    throw new Exception("No valid backup folder.");
+
+                XmlSerializer serializer = new XmlSerializer(typeof (plist));
+
+                plist info = (plist) serializer.Deserialize(File.OpenRead(infoFile));
+
+                if (info.Item.GetType() != typeof (dict))
+                    return null;
+
+                dict _dict = (dict) info.Item;
+
+                Dictionary<string, string> ret = new Dictionary<string, string>();
+
+                for (var i = 0; i < _dict.key.Length; i++)
+                {
+                    ret.Add(_dict.key[i], _dict.Items[i].ToString());
+                }
+                return ret;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
